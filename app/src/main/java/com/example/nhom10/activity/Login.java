5abf6c4177f777d1389821b2d0f452d725b2cfdc
tkstung03 <thanhtungph03.it@gmail.com>
2024-12-06 00:54:05 +0700

    add login,register

---
+++ app/src/main/java/com/example/nhom10/activity/Login.java
@@ -0,0 +1,38 @@
+package com.example.nhom10.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.nhom10.R;
+
+public class Login extends AppCompatActivity {
+
+    TextView register;
+    Button btnLogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        register = findViewById(R.id.register_prompt);
+        btnLogin = findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent2 = new Intent(Login.this, Register.class);
+                startActivity(intent2);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/nhom10/activity/Register.java
@@ -0,0 +1,39 @@
+package com.example.nhom10.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.nhom10.R;
+
+public class Register extends AppCompatActivity {
+    Button btnRegister;
+    TextView txtLogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        btnRegister = findViewById(R.id.btn_register_register);
+        txtLogin = findViewById(R.id.login_prompt);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Register.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        txtLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent1 = new Intent(Register.this, Login.class);
+                startActivity(intent1);
+            }
+        });
+    }
+}
